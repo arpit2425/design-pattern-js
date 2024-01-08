58c2ca49d6463ac2532f6a5f5052325ae6aef9d7
arpit2425 <arpittrivedi2425@gmail.com>
2024-01-08 18:01:06 +0530

    add mediator pattern

--- behaviouralPatterns/mediatorPattern.js
+++ behaviouralPatterns/mediatorPattern.js
@@ -1,3 +1,27 @@
 /* A downside would be that now we have introduced a single point of failure into our system, meaning if our mediator fails, the entire system could stop working.
-*/
-
+ */
+class ChatRoom {
+  constructor() {}
+  showMessage(user, message) {
+    let name = user.getName();
+    let time = new Date();
+    console.log(`${time} [${name}]: ${message}`);
+  }
+}
+class User {
+  constructor(name, chatRoom) {
+    this.name = name;
+    this.chatRoom = chatRoom;
+  }
+  getName() {
+    return this.name;
+  }
+  send(message) {
+    this.chatRoom.showMessage(this, message);
+  }
+}
+const mediator = new ChatRoom();
+const jane = new User('Jane', mediator);
+const john = new User('John', mediator);
+jane.send('Hi John');
+john.send('Hii');
